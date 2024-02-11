0768d136fcb6c3cb6ff9830a103e4ad7383c8a15
Letícia T. Hashimoto Ono <67491329+letyhashimoto@users.noreply.github.com>
2024-02-11 00:39:00 -0300

    Initial commit

---
+++ nlw-expert-rn/src/app/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text } from "react-native"
+
+export default function Home(){
+    return (
+        <View className="pt-8">             
+            <Text className="text-white text-2xl">React Native!</Text>
+        </View>
+    )
+}
