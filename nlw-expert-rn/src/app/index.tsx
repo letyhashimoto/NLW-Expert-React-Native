31c2c56ff5ed2c480104aad14f67e39769171374
Letícia T. Hashimoto Ono <67491329+letyhashimoto@users.noreply.github.com>
2024-02-11 01:34:13 -0300

    deletando
    deletando arquivos duplicados

--- nlw-expert-rn/src/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import {View, Text } from "react-native"
-
-export default function Home(){
-    return (
-        <View className="pt-8">             
-            <Text className="text-white text-2xl">React Native!</Text>
-        </View>
-    )
-}
